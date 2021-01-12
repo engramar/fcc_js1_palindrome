18da07174cea97fb8d29349674c98431dfba4004
Engramar Bollas <engramar.bollas@gmail.com>
2021-01-12 20:55:32 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Palindrome = (word) => {
     let trimmed_word = word.replace(/[\W_]+/g,"").toLowerCase();    
     let reversed_trimmed_word = trimmed_word.split("").reverse().join("");
-    console.log(trimmed_word, reversed_trimmed_word)
     if (trimmed_word === reversed_trimmed_word ) {
         return true;
     } else {
